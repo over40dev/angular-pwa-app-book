ba95bb90b9d5f44f5f0c3989ee43ecb1ee5790bd
Cogentx <cogentx@live.com>
2019-10-31 21:16:08 -0230

    kanban--base compoents and routes

---
+++ src/app/shared/shell/shell.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss'],
+})
+export class ShellComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public afAuth: AngularFireAuth
+  ) {}
+}
