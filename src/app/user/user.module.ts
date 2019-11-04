dc9ef9ee71aead4f25f0c5afa3b2f55b66d802ed
Cogentx <cogentx@live.com>
2019-11-03 21:58:15 -0330

    kanban-L17-email-login-initial

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { UserRoutingModule } from './user-routing.module';
+import { EmailLoginComponent } from './email-login/email-login.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { GoogleSigninDirective } from './google-signin.directive';
 
 
 @NgModule({
-  declarations: [LoginPageComponent, GoogleSigninDirective],
+  declarations: [LoginPageComponent, GoogleSigninDirective, EmailLoginComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
     SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
