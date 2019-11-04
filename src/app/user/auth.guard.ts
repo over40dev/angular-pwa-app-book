7e339836b19adb27934ebbc2d3134a3783991df7
Cogentx <cogentx@live.com>
2019-11-04 18:31:07 -0330

    kanban-L18-auth guard (w)

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot  } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Observable } from 'rxjs';
+import { SnackService } from '../services/snack.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private snack: SnackService,
+    ) {}
 
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
     ): Promise<boolean> {
 
-      const user = this.afAuth.auth.currentUser;
+      const user = await this.afAuth.auth.currentUser;
       const isLoggedIn = !!user;
+      if (!isLoggedIn) {
+        this.snack.authError();
+      }
 
-      
-    
-    
       return isLoggedIn;
   }
   
