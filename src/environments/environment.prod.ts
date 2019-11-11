90bd834b5092bb5bf93c5f553e12aab5620c05a1
Cogentx <cogentx@live.com>
2019-11-11 10:26:42 -0330

    tic-tac-toe---initial setup (w)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDN350yTjwRvMCB6CBB-r4RcghWLtgVwi4',
-    authDomain: 'cogentx-kanban.firebaseapp.com',
-    databaseURL: 'https://cogentx-kanban.firebaseio.com',
-    projectId: 'cogentx-kanban',
-    storageBucket: 'cogentx-kanban.appspot.com',
-    messagingSenderId: '859078331059',
-    appId: '1:859078331059:web:6897a2a9dadc9da3883ec6',
-    measurementId: 'G-GBWQMBBJV6',
-  },
+    apiKey: 'AIzaSyD2Lql_zxAl1MUtIFqnn3ZjBOMcx8aVp8M',
+    authDomain: 'launchpad-pwa.firebaseapp.com',
+    databaseURL: 'https://launchpad-pwa.firebaseio.com',
+    projectId: 'launchpad-pwa',
+    storageBucket: 'launchpad-pwa.appspot.com',
+    messagingSenderId: '117098370902',
+    appId: '1:117098370902:web:c96a654857a0e36c66a8f7',
+    measurementId: 'G-9S7BHZBDG2'
+  }
 };
